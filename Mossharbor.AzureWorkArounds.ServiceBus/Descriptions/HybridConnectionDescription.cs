067acf26abb879ef939150f78ef5d7118f3f5661
Matthew Johnson <mattj@mossharbor.com>
2018-06-04 15:53:42 -0700

    Moved Description files to their own forlder

---
+++ Mossharbor.AzureWorkArounds.ServiceBus/Descriptions/HybridConnectionDescription.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mossharbor.AzureWorkArounds.ServiceBus
+{
+    public class HybridConnectionDescription
+    {
+    }
+}
